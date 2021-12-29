41d29447b6cb6f2921f7fc163922279f159da8f5
Carolaine Vieira <54178722+carolaine-vieira@users.noreply.github.com>
2021-12-29 12:57:10 -0300

    home page media queries

--- page.php
+++ page.php
@@ -34,20 +34,7 @@
           ?>
                 <div class="post-single">          
                   <?php the_content(); ?>
-                </div>        
-
-                <div class="permalink-navigation">
-                  <?php
-                    the_post_navigation(array(
-                      'next_text' => '<span class="meta-nav" aria-hidden="true">' . __('Next', 'twentyfifteen') . '</span> ' .
-                          '<span class="screen-reader-text">' . __('Next post:', 'twentyfifteen') . '</span> ' .
-                          '<span class="post-title">%title</span>',
-                      'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __('Previous', 'twentyfifteen') . '</span> ' .
-                          '<span class="screen-reader-text">' . __('Previous post:', 'twentyfifteen') . '</span> ' .
-                          '<span class="post-title">%title</span>',
-                    ));
-                  ?>
-                </div>
+                </div>   
 
               <?php
                 endwhile;
