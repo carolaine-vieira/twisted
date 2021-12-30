5b1c5d0805cad8555c6d5d5ab801516bb1f4227d
Carolaine Vieira <54178722+carolaine-vieira@users.noreply.github.com>
2021-12-30 00:54:46 -0300

    Adding hooks

--- functions.php
+++ functions.php
@@ -1,35 +1,69 @@
 <?php
 
-// adding theme support
-add_theme_support('title-tag');
-add_theme_support('post-thumbnails');
-add_theme_support('post-formats', array( 'gallery', 'quote', 'image', 'video' ));
-
-// custom header menu
-$args = array(
-  'flex-width'    => true,
-  'width'         => 1500,
-  'flex-height'   => true,
-  'height'        => 1000,
-  'default-image' => get_template_directory_uri() . '/assets/images/first-section-bg.jpg',
-);
-add_theme_support('custom-header', $args);
-
-// custom menus
-function register_my_menus() {
-   register_nav_menus(
-    array(
-      'header-menu' => __('Header Menu'),
-      'footer-menu' => __('Footer Menu')
-    )
-  );
+function wpdocs_theme_name_scripts() {
+	wp_enqueue_style('global template', get_template_directory_uri().'/assets/css/style.css', array(), '1.0', 'all');	
+  wp_enqueue_script('global scripts', get_template_directory_uri().'/assets/js/script.js', array(), '1.0', 'all');
+  wp_enqueue_script('isotope masonry', 'https://npmcdn.com/isotope-layout@3.0.6/dist/isotope.pkgd.js', array(), '3.0.6', 'all');
+	wp_enqueue_script('awesome icons', "https://kit.fontawesome.com/eb5e14c15e.js", array(), '5.15', 'all');
 }
-add_action('init', 'register_my_menus');
+add_action('wp_enqueue_scripts', 'wpdocs_theme_name_scripts');
 
-// remove width & height attributes from images
-function remove_img_attr ($html) {
-  return preg_replace('/(width|height)="\d+"\s/', "", $html);
+if ( ! function_exists( 'twisted_setup' ) ) {
+  function twisted_setup() {
+    // adding theme support
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('post-formats', array('image'));
+    add_theme_support( 'editor-styles' );
+
+    // custom header menu
+    $args = array(
+      'flex-width'    => true,
+      'width'         => 1500,
+      'flex-height'   => true,
+      'height'        => 1000,
+      'default-image' => get_template_directory_uri() . '/assets/images/first-section-bg.jpg',
+    );
+    add_theme_support('custom-header', $args);
+
+    // custom menus
+    function register_my_menus() {
+      register_nav_menus(
+        array(
+          'header-menu' => __('Header Menu'),
+          'footer-menu' => __('Footer Menu')
+        )
+      );
+    }
+    add_action('init', 'register_my_menus');
+
+    // remove width & height attributes from images
+    function remove_img_attr ($html) {
+      return preg_replace('/(width|height)="\d+"\s/', "", $html);
+    }
+    
+    add_filter( 'post_thumbnail_html', 'remove_img_attr' );
+
+    // Custom background color.
+    add_theme_support(
+      'custom-background',
+      array(
+        'default-color' => 'd1e4dd',
+      )
+    );
+
+    // Editor color palette.
+    $black     = '#000000';
+    $dark_gray = '#28303D';
+    $gray      = '#39414D';
+    $green     = '#D1E4DD';
+    $blue      = '#D1DFE4';
+    $purple    = '#D1D1E4';
+    $red       = '#E4D1D1';
+    $orange    = '#E4DAD1';
+    $yellow    = '#EEEADD';
+    $white     = '#FFFFFF';
+  }
 }
- 
-add_filter( 'post_thumbnail_html', 'remove_img_attr' );
+add_action( 'after_setup_theme', 'twisted_setup' );
 
--- header.php
+++ header.php
@@ -2,31 +2,9 @@
 <html class="no-js" <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width">
-    
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>      
-    <script src="https://npmcdn.com/isotope-layout@3/dist/isotope.pkgd.js"></script>    
+    <meta name="viewport" content="width=device-width">    
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>  
     <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/simple-form.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/script.js" defer></script>
-    <script src="https://kit.fontawesome.com/eb5e14c15e.js" crossorigin="anonymous"></script>
-
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&family=Playfair+Display:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet" />   
-
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/style.css" />    
-<?php
-  if (is_single()) {
-?>
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/single-post.css" />
-<?php
-  } elseif (is_page()) {
-?>
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/page.css" />
-<?php
-  }
-?>
-
     <?php wp_head(); ?>
   </head>
 
