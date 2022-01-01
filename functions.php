2d6bd60afe59c2c9f313e34a01cd148728025e37
Carolaine Vieira <54178722+carolaine-vieira@users.noreply.github.com>
2022-01-01 19:39:47 -0300

    Minified style

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 // Enqueue scripts and styles
 function twisted_theme_scripts() {
-  wp_enqueue_style('global template', get_template_directory_uri().'/assets/css/style.css', array(), '1.0', 'all');	
+  wp_enqueue_style('global template', get_template_directory_uri().'/assets/css/css-dist/style.min.css', array(), '1.0', 'all');	
   wp_enqueue_script('global scripts', get_template_directory_uri().'/assets/js/script.js', array(), '1.0', 'all');
   wp_enqueue_script('isotope masonry', 'https://npmcdn.com/isotope-layout@3.0.6/dist/isotope.pkgd.js', array(), '3.0.6', 'all');
   wp_enqueue_script('awesome icons', "https://kit.fontawesome.com/eb5e14c15e.js", array(), '5.15', 'all');
